a2eb7e13885475cbd2a79cd86d3001a4a5121e2f
abdu89 <abdualoui@hotmail.de>
2015-11-18 15:54:49 +0100

    java datein Datein hochgelden

---
+++ Tagung.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;  
+  
+public class Tagung<Tagung> { 
+ 	private Tagung tagung; 
+ 	private Tagung [] teilnehmer; 
+ 	private Tagung [] workshops; 
+ 	private Tagung[] rahmenprogramm; 
+	private Tagung [] referent; 
+ 	private Tagung wert; 
+ 	 
+ 	Scanner scanner = new Scanner(System.in); 
+ 	public Tagung(){ 
+ 		 
+        } 
+ 	public Tagung(Tagung tagung){ 
+ 		this.wert=wert; 
+ 		this.tagung=tagung; 
+ 		 
+	} 
+        
+ 
+ 	 
+	public Tagung getTagung(){ 
+ 		return tagung; 
+	} 
+ 	public void setTagung(Tagung tagung){ 
+ 		this.tagung=tagung; 
+ 	} 
+ 
+ 
+ 	public void addTeilnehmer(Tagung[] teilnehmer){ 
+ 		System.out.println("Bitte einen Teilnehmernamen eingeben: "); 
+ 		String tn = scanner.next(); 
+ 		for(int i =0; i<teilnehmer.length;i++){ 
+ 			 
+ 			teilnehmer[i]=(Tagung) tn; 
+ 			System.out.println(i); 
+ 
+ 
+ 		} 
+ 
+ 
+	} 
+ 	public void addWorkshops(Tagung[] workshops){ 
+ 	} 
+ 	public void addRahmenprogramm(Tagung[] rahmenprogramm){ 
+ 	} 
+ 	public void addReferent(Tagung[] referent){ 
+	} 
+ 	 
+ 
+ 
+
+ 
+ } 
